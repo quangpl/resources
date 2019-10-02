1c78f143a804592ed35250939dc079061ab73be0
Phan Quang <phanlequang99@gmail.com>
2019-10-02 09:47:27 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -17,7 +17,7 @@ function addElement() {
   // and give it popup content
   var newDiv = document.createElement("div");
   newDiv.innerHTML +=
-    '<div id="popup" style="margin: 0 auto;  position: relative;width: 750px; height:430px;z-index: 999;display: none;top:0;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank" href="https://google.vn"><img src="https://i.imgur.com/jiRfV84.png"/></a></div>';
+    '<div id="popup" style="margin: 0 auto;  position: relative;width: 750px; height:430px;z-index: 999;display: none;top:0;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank" href="https://google.vn"><img src="https://i.imgur.com/tgFmFXp.png"/></a></div>';
 
   // add the newly created element and its content into the DOM
   var currentDiv = document.getElementById("main_container");
