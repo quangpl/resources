39856207cad2b7ebee07d6124e3cfde42e8e59ff
Phan Quang <phanlequang99@gmail.com>
2019-10-05 23:44:58 +0700

    update script

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-
 console.log(window.location.hostname);
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -8,7 +7,7 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open(
   "GET",
-  "http://localhost:3000/api/ads?url=devnhanhgonle.myharavan.com",
+  `https://angelic-edition-253418.appspot.com/api/ads?url=${window.location.hostname}`,
   true
 );
 xhttp.send();
@@ -16,13 +15,19 @@ function openPopup() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML = this.responseText;
       console.log(this.responseText);
+
+      const expire = localStorage.getItem("expire");
+      console.log(new Date().getTime() / 1000 + 86400);
+
+      if (!expire || Date.now() > expire) {
+        addElement();
+        localStorage.setItem("expire", new Date().getTime() / 1000 + 86400); //1day
+      }
     }
   };
   xhttp.open("GET", "xmlhttp_info.txt", true);
   xhttp.send();
-
 
   var el = document.getElementById("popup");
   el.style.display = "block";
@@ -37,14 +42,13 @@ function closePopup() {
   document.body.style.background = "white";
 }
 
-function addElement() {
+function addElement(url) {
   // create a new div element
   // and give it popup content
   var newDiv = document.createElement("div");
-  newDiv.innerHTML +=
-    '<div id="popup" style="margin: 0 auto;  position: fixed;width: 750px; height:460px;z-index: 9999999;display: flex;align-items: center;justify-content: center;;none;top:100px;left:300px;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank" href="/collections/xa-hang-ton-kho"><img src="https://i.imgur.com/fBcfDGX.png"/></a></div>';
-  const html =
-    '<div id="popup" style="margin: 0 auto;  position: relative;width: 750px; height:460px;z-index: 999;display: none;top:0;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank"  href="/collections/xa-hang-ton-kho"><img src="https://i.imgur.com/fBcfDGX.png"/></a></div>';
+  newDiv.innerHTML += `<div id="popup" style="margin: 0 auto;  position: fixed;width: 750px; height:460px;z-index: 9999999;display: flex;align-items: center;justify-content: center;;none;top:100px;left:300px;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank" href="/collections/$${url}"><img src="https://i.imgur.com/fBcfDGX.png"/></a></div>`;
+
+  const html = `<div id="popup" style="margin: 0 auto;  position: relative;width: 750px; height:460px;z-index: 999;display: none;top:0;background-color: #fff;  border: 1px solid #ddd;  border-radius: 5px;  box-shadow: 0 2px 8px #aaa;  overflow: hidden;"><span style=" float:right;cursor: pointer;font-weight: bold;position: absolute;right: 9px;top: 5px; font-size:24px; color:white;" onClick="closePopup()">X</span><a target="_blank"  href="/collections/${url}"><img src="https://i.imgur.com/fBcfDGX.png"/></a></div>`;
 
   // add the newly created element and its content into the DOM
   document.body.insertBefore(newDiv, document.body.childNodes[0]);
@@ -52,9 +56,5 @@ function addElement() {
   // open popup onload
   openPopup();
 }
-const expire = localStorage.getItem("expire");
-console.log(new Date().getTime() / 1000 + 86400);
-if (!expire || Date.now() > expire) {
-  addElement();
-  localStorage.setItem("expire", new Date().getTime() / 1000 + 86400); //1day
-}
+
+url_collection;
